19a714007af9c2dc9795932af39f411ff4c0fda2
Gyorgy David <gyorgy.david2006@gmail.com>
2026-02-24 10:40:40 +0100

    folyt

--- 08-erett/main.dart
+++ 08-erett/main.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-void main(){
+void main() {
   File file = File('08-erett/rendel.txt');
   List<String> contents = file.readAsLinesSync();
-  
+
   List<Map<String, dynamic>> rendelesek = [];
-  for(var row in contents){
+  for (var row in contents) {
     var line = row.split(" ");
 
     rendelesek.add({
       "nap": int.parse(line[0]),
       "tipus": line[1],
-      "mennyiseg": int.parse(line[2])
+      "mennyiseg": int.parse(line[2]),
     });
   }
 
@@ -21,17 +21,66 @@ void main(){
   print("3. feladat:");
   print("Kérem adjon meg egy napot: ");
   var day = int.parse(stdin.readLineSync()!);
-  print("A rendelések száma az adott napon: ${rendelesek.where((x) => x["nap"] == day).length}");
+  print(
+    "A rendelések száma az adott napon: ${rendelesek.where((x) => x["nap"] == day).length}",
+  );
 
   print("4. feladat:");
   var NR = rendelesek.where((x) => x["tipus"] == "NR");
   Set<int> napok = {};
-  for(var rendeles in NR){
+  for (var rendeles in NR) {
     napok.add(rendeles["nap"]);
   }
   var calc = 30 - napok.length;
-  if(calc == 0) 
+  if (calc == 0)
     print("Minden nap volt rendelés a reklámban nem érintett városból");
   else
     print("$calc nap nem volt a reklámban nem érintett városból rendelés");
+
+  print("5. feladat");
+  var maxDarab = rendelesek[0];
+  rendelesek.forEach((x) {
+    if (x["mennyiseg"] > maxDarab["mennyiseg"]) {
+      maxDarab = x;
+    }
+  });
+  print(
+    "A legnagyobb darabszám: ${maxDarab["mennyiseg"]}, a rendelés napja: ${maxDarab["nap"]}",
+  );
+
+  print("6. feladat");
+  print("Település: ");
+  var telepules = stdin.readLineSync()!;
+  print("Nap: ");
+  var nap = int.parse(stdin.readLineSync()!);
+  print("Összes: ${osszes(telepules, nap, rendelesek)}");
+
+  print("7. feladat");
+  var pl = osszes("PL", 21, rendelesek);
+  var tv = osszes("TV", 21, rendelesek);
+  var nr = osszes("NR", 21, rendelesek);
+  print("A rendelt termékek darabszáma a 21. napon PL: $pl TV: $tv NR: $nr");
+
+  print("8. feladat");
+  String kimenet = "Napok 1..10 11..20 21..30\n";
+  List<int> darabszamokPL = [0, 0, 0];
+  List<int> darabszamokTV = [0, 0, 0];
+  List<int> darabszamokNR = [0, 0, 0];
+  for (var i = 0; i < 31; i++) {
+    darabszamokPL[i ~/ 10] += osszes("PL", i, rendelesek);
+    darabszamokTV[i ~/ 10] += osszes("TV", i, rendelesek);
+    darabszamokNR[i ~/ 10] += osszes("NR", i, rendelesek);
+  }
+}
+
+int osszes(String varos, int nap, List<Map<String, dynamic>> lista) {
+  int sum = 0;
+
+  for (var x in lista) {
+    if (x["nap"] == nap && x["tipus"] == varos) {
+      sum += x["mennyiseg"] as int;
+    }
+  }
+
+  return sum;
 }
